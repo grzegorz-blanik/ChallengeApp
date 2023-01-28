16303384fcfc383c3cb491541ad81d1049aeaa5a
Grzegorz Błanik <grzegorz.blanik@hotmail.com>
2023-01-29 00:58:57 +0100

    Dzień 4 - zadanie domowe

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,9 +1,8 @@
 ﻿string name = "Ewa";
-bool male = true;
 bool female = true;
 int age = 17;
 
-if (female == true && age < 30)
+if (female && age < 30)
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
@@ -11,7 +10,7 @@ else if (name == "Ewa" && age == 33)
 {
     Console.WriteLine("Ewa, lat 33");
 }
-if (male == true && age < 18)
+if (!female && age < 18)
 {
     Console.WriteLine("Niepełnoletni Mężczyzna");
 }
