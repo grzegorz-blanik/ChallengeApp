12a7de618e92ce674079ba9c8159fa0a04398b83
Grzegorz Błanik <grzegorz.blanik@hotmail.com>
2023-02-04 12:17:31 +0100

    Dzień 6 - zadanie domowe

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,71 +1,46 @@
-﻿int number = 5799;
-string numberAsString = number.ToString();
-char[] signs = numberAsString.ToArray();
+﻿using ConsoleApp1;
+Employee employee1 = new Employee("Jan", "Kowalski", "21");
+Employee employee2 = new Employee("Marek", "Nowak", "30");
+Employee employee3 = new Employee("Anna", "Wójcik", "25");
 
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+employee1.AddScore(5);
+employee1.AddScore(9);
+employee1.AddScore(7);
+employee1.AddScore(6);
+employee1.AddScore(10);
 
-foreach (char sign in signs)
+employee2.AddScore(1);
+employee2.AddScore(3);
+employee2.AddScore(8);
+employee2.AddScore(10);
+employee2.AddScore(2);
+
+employee3.AddScore(7);
+employee3.AddScore(3);
+employee3.AddScore(6);
+employee3.AddScore(9);
+employee3.AddScore(5);
+
+List<Employee> employees = new List<Employee>()
 {
-    if (sign == '0')
-    {
-        counter0++;
-    }
-    else if (sign == '1')
-    {
-        counter1++;
-    }
-    else if (sign == '2')
-    {
-        counter2++;
-    }
-    else if (sign == '3')
-    {
-        counter3++;
-    }
-    else if (sign == '4')
-    {
-        counter4++;
-    }
-    else if (sign == '5')
-    {
-        counter5++;
-    }
-    else if (sign == '6')
-    {
-        counter6++;
-    }
-    else if (sign == '7')
-    {
-        counter7++;
-    }
-    else if (sign == '8')
-    {
-        counter8++;
-    }
-    else if (sign == '9')
-    {
-        counter9++;
-    }
+    employee1, employee2, employee3
+};
 
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach (var employee in employees)
+{
+    if (employee.Result > maxResult)
+    {
+        maxResult = employee.Result;
+        employeeWithMaxResult = employee;
+    }
 }
 
-Console.WriteLine($"Wynik dla liczby: {number}");
-Console.WriteLine($"0 => {counter0}");
-Console.WriteLine($"1 => {counter1}");
-Console.WriteLine($"2 => {counter2}");
-Console.WriteLine($"3 => {counter3}");
-Console.WriteLine($"4 => {counter4}");
-Console.WriteLine($"5 => {counter5}");
-Console.WriteLine($"6 => {counter6}");
-Console.WriteLine($"7 => {counter7}");
-Console.WriteLine($"8 => {counter8}");
-Console.WriteLine($"9 => {counter9}");
+Console.Write("Proacownik z najlepszym wynikiem, to: ");
+Console.Write(employeeWithMaxResult.Name + " ");
+Console.Write(employeeWithMaxResult.Surname + ", ");
+Console.Write(employeeWithMaxResult.Age + " lat, ");
+Console.Write($"Wynik: {employeeWithMaxResult.Result} pkt.");
+Console.WriteLine();
